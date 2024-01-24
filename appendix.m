17d2c431ccb9fc5b279f2b53d252632e1c153618
Filip Najman <78727749+F-Najman@users.noreply.github.com>
2024-01-24 10:12:57 +0100

    Add files via upload
    Code written by Maarten to verify the claims in the Theorem in the appendix.

---
+++ appendix.m
@@ -0,0 +1,17 @@
+// This code verifies the claim in the proof of the Theorem in the Appendix that the ramification points X_0(37)->J_0(37)^- have j-invariant 287496.
+
+
+C := SmallModularCurve(37);
+G,b,_:=AutomorphismGroup(C);
+for g in G do;
+	Cquot, f := CurveQuotient(AutomorphismGroup(C,[b(g)]));
+	if Genus(Cquot) ne 1 then continue; end if;
+	//X_0(37)^+ has j invariant 110592/37
+	//J_0(37)^- has j invariant 1404928000/50653
+	if jInvariant(Cquot) eq 110592/37 then continue; end if;
+	assert jInvariant(Cquot) eq 1404928000/50653;
+	ram := Support(RamificationDivisor(f))[1];
+	assert Degree(ram) eq 2;
+	j := jInvariant(ram,37);
+	print "j", j, MinimalPolynomial(j);
+end for;
